6cc6e8b51b88b56d19ddfe250729ca6a5d55b5dc
lcsway <lucas.nascimento4213@gmail.com>
2023-03-07 15:38:58 -0300

    Shopping Cart Screen added

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import ProductsScreen from './src/screens/ProductScreen';
 import ProductsDetailsScreen from './src/screens/ProductDetailsScreen';
+import ShoppingCart from './src/screens/ShoppingCart';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ProductsScreen /> */}
-      <ProductsDetailsScreen />
+      {/* <ProductsDetailsScreen /> */}
+      <ShoppingCart/>
       <StatusBar style="auto" />
     </View>
   );

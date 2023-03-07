f2957197db086a8ff4b9240c4729c6d223a31944
lcsway <lucas.nascimento4213@gmail.com>
2023-03-07 17:56:36 -0300

    Navigate

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import ProductsScreen from './src/screens/ProductScreen';
-import ProductsDetailsScreen from './src/screens/ProductDetailsScreen';
-import ShoppingCart from './src/screens/ShoppingCart';
+import Navigation from './src/navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <ProductsScreen /> */}
-      {/* <ProductsDetailsScreen /> */}
-      <ShoppingCart/>
+      <Navigation/>
       <StatusBar style="auto" />
     </View>
   );
